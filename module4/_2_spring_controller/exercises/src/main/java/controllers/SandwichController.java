f0f7805c833cd3b3e6ccbcac6f11d94ed9ffe9bf
phatthanh113 <phat.thanh.113@gmail.com>
2021-02-26 17:01:21 +0700

    finish exercise sandwich

---
+++ module4/_2_spring_controller/exercises/src/main/java/controllers/SandwichController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/sandwich")
+public class SandwichController {
+    @GetMapping("/")
+    public String formCheckBox() {
+        return "sandwich/index";
+    }
+    @PostMapping("/result")
+    public String resultCheckBox(@RequestParam(required = false) String[] condiments, Model model) {
+        if(condiments != null) {
+        model.addAttribute("message","Your Choose");
+        model.addAttribute("results",condiments);
+        }else {
+            model.addAttribute("message","không có sự lựa chọn nào");
+        }
+        return "sandwich/index";
+    }
+}
